67aaccc26d0dee53221fbcb829b9b7f259a21859
VitaliiPytymko <vitaliipytymko@gmail.com>
2024-05-23 21:12:23 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket= new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var endPoint = new IPEndPoint(IPAddress.Loopback, 1234);
+            try
+            {
+                Console.WriteLine("Client has started");
+                socket.Connect(endPoint);
+                Console.WriteLine("Напишіть 'DATA'  щоб отримати дату, або 'TIME' щоб отримати час.");
+                string message = Console.ReadLine();
+                socket.Send(Encoding.UTF8.GetBytes(message));
+                var buf= new byte[1024];
+                int bytesRead= socket.Receive(buf);
+                string responseMessage= Encoding.UTF8.GetString(buf, 0, bytesRead);
+                Console.WriteLine($"Отримано від {socket?.RemoteEndPoint?.ToString()}: {responseMessage}");
+                socket?.Shutdown(SocketShutdown.Both);
+                socket?.Close();
+
+            }
+            catch(Exception ex) { Console.WriteLine(ex.Message); }
+            Console.WriteLine("\nНатисни будь яку кнопку щоб вийти....");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server&Client2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var endPoint = new IPEndPoint(IPAddress.Any, 1234);
+            socket.Bind(endPoint);
+            socket.Listen(10);
+            try 
+            {
+                Console.WriteLine("Server has started");
+                while (true)
+                {
+                    var client = socket.Accept();
+                    var buf = new byte[1024];
+                    int bytesRead = client.Receive(buf);
+                    string receivedMessage = Encoding.UTF8.GetString(buf, 0, bytesRead);
+                    Console.WriteLine($"Отримано від  {client?.RemoteEndPoint?.ToString()}: {receivedMessage}");
+
+                    string responseMessage;
+                    if (receivedMessage.Trim().ToUpper() == "DATA")
+                    {
+                        responseMessage = DateTime.Now.ToString("yyy-MM-dd");
+                    }
+                    else if (receivedMessage.Trim().ToUpper() == "TIME")
+                    {
+                        responseMessage = DateTime.Now.ToString("HH:mm:ss");
+                    }
+                    else
+                    {
+                        responseMessage = "Invalid request";
+                    }
+                    client?.Send(Encoding.UTF8.GetBytes(responseMessage));
+                    client?.Shutdown(SocketShutdown.Both);
+                    client?.Close();
+                }
+            }
+            catch ( Exception ex) { Console.WriteLine(ex.Message); }
+            socket.Close();
+            Console.WriteLine("\nPress any key to exit....");
+            Console.ReadKey();
+        }
+    }
+}
